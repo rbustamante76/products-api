e24b84760235585a778a458267d67b6f1913b827
robustamante <rodrigo.bustamante@walmart.com>
2020-12-29 23:38:09 -0300

    modify db url connection

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 exports.connect = function(){
-  return mongoose.connect('mongodb://brandDiscountsUser:brandDiscountsPassword@cluster0-shard-00-02.swo48.mongodb.net:27017?retryWrites=true', {
+  return mongoose.connect('mongodb+srv://brandDiscountsUser:brandDiscountsPassword@cluster0.swo48.mongodb.net/promotions?retryWrites=true&w=majority', {
       promiseLibrary: global.Promise,
       useNewUrlParser: true,
       useCreateIndex: true,
-      useUnifiedTopology: true,
-      dbName: 'promotions'
+      useUnifiedTopology: true
     },
     function(err, res) {
       if(err) throw err;
